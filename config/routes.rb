4c212e630b8a6f82f66f63b2a02ab979a471ee00
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2025-03-26 01:53:25 +0900

    add: sessions_controller & set routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # ユーザー関連
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
